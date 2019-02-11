b8e4529ee1e440165e49d52678afd30e9dd2be9a
Paul Huf <paul.huf@codesmithing.com>
2019-02-10 23:18:23 -0500

    Refactor agent

--- agent.cc
+++ agent.cc
@@ -14,37 +14,60 @@ RETSIGTYPE stop_server(int a) {
 }
 
 int Agent::run(int argc, char** argv) {
-    int agentx_subagent = 0;
-    int background = 0;
-    int syslog = 0;
+    parse_command_line(argc, argv);
 
-    if (syslog)
+    if (syslog_)
         snmp_enable_calllog();
     else
         snmp_enable_stderrlog();
 
-    if (agentx_subagent) {
+    if (agentx_subagent_) {
         netsnmp_ds_set_boolean(NETSNMP_DS_APPLICATION_ID, NETSNMP_DS_AGENT_ROLE, 1);
     }
 
-    if (background && netsnmp_daemonize(1, !syslog))
+    if (background_ && netsnmp_daemonize(1, !syslog))
         exit(1);
-
-    SOCK_STARTUP;
 
     init_agent("poc-snmpd");
     init_randomValue();
 
-    if (!agentx_subagent) {
+    if (!agentx_subagent_) {
         init_vacm();
         init_usm();
     }
 
     init_snmp("poc-snmpd");
 
-    if (!agentx_subagent)
+    if (!agentx_subagent_)
         init_master_agent();
 
+    event_loop();
+    snmp_shutdown("poc-snmpd");
+
+    return 0;
+}
+
+void Agent::parse_command_line(int argc, char** argv) {
+    int c;
+
+    while ((c == getopt(argc, argv, "bsx:"))) {
+        switch (c) {
+        case 'b':
+            background_ = 1;
+            break;
+
+        case 's':
+            syslog_ = 1;
+            break;
+
+        case 'x':
+            agentx_subagent_ = 1;
+            break;
+        }
+    }
+}
+
+void Agent::event_loop() {
     keep_running = 1;
     signal(SIGTERM, stop_server);
     signal(SIGINT, stop_server);
@@ -54,8 +77,4 @@ int Agent::run(int argc, char** argv) {
     while (keep_running) {
         agent_check_and_process(1);
     }
-
-    snmp_shutdown("poc-snmpd");
-
-    return 0;
 }
--- agent.h
+++ agent.h
@@ -3,5 +3,11 @@ public:
     int run(int argc, char** argv);
 
 private:
-    void process_command_line(int argc, char** argv);
+    int background_ = 0;
+    int syslog_ = 0;
+    int agentx_subagent_ = 0;
+
+    void parse_command_line(int argc, char** argv);
+
+    void event_loop();
 };
